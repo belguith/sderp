81c9130896bf2907fad8853dcf3c13e3083afcc8
Skander <Skander@Skander-PC>
2017-04-09 03:33:42 +0100

    Fiche de paie details ok

---
+++ sderp-web/src/main/java/beans/viewBeans/FicheBean.java
@@ -0,0 +1,169 @@
+package beans.viewBeans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import beans.sessionBeans.GlobalBean;
+import entities.FicheDePaie;
+import services.impl.FicheDePaieService;
+import utils.SalaryCalculations;
+
+@ManagedBean(name = "fichebean")
+@ViewScoped
+public class FicheBean {
+
+	@EJB
+	FicheDePaieService ficheDePaieLocal;
+	private List<FicheDePaie> listFicheDePaie;
+	private FicheDePaie selectedFicheDePaie;
+	private Integer selectedFicheDePaieID;
+	private Double salaireBrut;
+	private Double salaireNet;
+	private Double impotSurRevenu;
+	private Double salaireImposable;
+	// private UserType userTypes;
+	// private UserType selectedUserType;
+	@ManagedProperty(value = "#{globalbean}")
+	private GlobalBean globalbean;
+
+	public FicheBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@PostConstruct
+	public void init() {
+		this.selectedFicheDePaie = new FicheDePaie();
+		this.listFicheDePaie = new ArrayList<>();
+		this.selectedFicheDePaieID = 2;// 0;
+		System.out.println("Init User");
+		if (this.globalbean.getSelectedFicheID() == 0) {
+			this.selectedFicheDePaieID = 2;// this.globalbean.getSelectedFicheID();
+			this.selectedFicheDePaie = ficheDePaieLocal.find(selectedFicheDePaieID);
+			this.salaireBrut = SalaryCalculations.salaireBruteMensuel(this.selectedFicheDePaie);
+			this.impotSurRevenu = SalaryCalculations.impoSurRevenue(this.selectedFicheDePaie);
+			this.salaireImposable = SalaryCalculations.salaireImposableMensuel(this.selectedFicheDePaie);
+			this.salaireNet = SalaryCalculations.salaireNet(this.selectedFicheDePaie);
+			System.out.println("Details OK");
+		}
+		System.out.println("Details OK");
+	}
+
+	public String doAddOrUpdateFicheDePaie() {
+		if (this.selectedFicheDePaieID == 0) {
+			this.selectedFicheDePaie = ficheDePaieLocal.create(this.selectedFicheDePaie);
+		} else if (this.selectedFicheDePaieID != 0) {
+			this.selectedFicheDePaie = ficheDePaieLocal.update(this.selectedFicheDePaie);
+		}
+		return this.globalbean.goToListFicheDePaie();
+	}
+
+	public String doResetForm() {
+		this.selectedFicheDePaieID = 0;
+		this.selectedFicheDePaie = new FicheDePaie();
+		this.globalbean.setSelectedFicheID(0);
+		return "";
+	}
+
+	/*********** GEtters and Setters ***********/
+	public List<FicheDePaie> getListFicheDePaie() {
+		return listFicheDePaie;
+	}
+
+	public void setListFicheDePaie(List<FicheDePaie> listFicheDePaie) {
+		this.listFicheDePaie = listFicheDePaie;
+	}
+
+	public Integer getSelectedFicheDePaieID() {
+		return selectedFicheDePaieID;
+	}
+
+	public void setSelectedFicheDePaieID(Integer selectedFicheDePaieID) {
+		this.selectedFicheDePaieID = selectedFicheDePaieID;
+	}
+
+	public FicheDePaie getSelectedFicheDePaie() {
+		return selectedFicheDePaie;
+	}
+
+	public void setSelectedFicheDePaie(FicheDePaie selectedFicheDePaie) {
+		this.selectedFicheDePaie = selectedFicheDePaie;
+	}
+
+	public GlobalBean getGlobalbean() {
+		return globalbean;
+	}
+
+	public void setGlobalbean(GlobalBean globalbean) {
+		this.globalbean = globalbean;
+	}
+
+	/**
+	 * @return the salaireBrut
+	 */
+	public Double getSalaireBrut() {
+		return salaireBrut;
+	}
+
+	/**
+	 * @param salaireBrut
+	 *            the salaireBrut to set
+	 */
+	public void setSalaireBrut(Double salaireBrut) {
+		this.salaireBrut = salaireBrut;
+	}
+
+	/**
+	 * @return the salaireNet
+	 */
+	public Double getSalaireNet() {
+		return salaireNet;
+	}
+
+	/**
+	 * @param salaireNet
+	 *            the salaireNet to set
+	 */
+	public void setSalaireNet(Double salaireNet) {
+		this.salaireNet = salaireNet;
+	}
+
+	/**
+	 * @return the impotSurRevenu
+	 */
+	public Double getImpotSurRevenu() {
+		return impotSurRevenu;
+	}
+
+	/**
+	 * @param impotSurRevenu
+	 *            the impotSurRevenu to set
+	 */
+	public void setImpotSurRevenu(Double impotSurRevenu) {
+		this.impotSurRevenu = impotSurRevenu;
+	}
+
+	/**
+	 * @return the salaireImposable
+	 */
+	public Double getSalaireImposable() {
+		return salaireImposable;
+	}
+
+	/**
+	 * @param salaireImposable
+	 *            the salaireImposable to set
+	 */
+	public void setSalaireImposable(Double salaireImposable) {
+		this.salaireImposable = salaireImposable;
+	}
+
+	
+	
+}
